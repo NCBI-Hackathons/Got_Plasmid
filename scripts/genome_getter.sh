68e5cd0a4b9105f48b77aea33020a0648ed3fc4a
Richard Copin <rickcopin@gmail.com>
2017-12-06 10:40:43 -0500

    Add files via upload

---
+++ scripts/genome_getter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in `cat ../assembly_ID/S_aureus_gapless_assemblies`; do wget `esearch -db assembly -query "$i" | efetch -format docsum | xtract -pattern DocumentSummary -element FtpPath_GenBank | 
+awk -F"/" '{print $0"/"$NF"_genomic.fna.gz"}'`; done 
+
+gunzip *.gz
+
+mv *.fna ../plasmid_fasta/.
+
