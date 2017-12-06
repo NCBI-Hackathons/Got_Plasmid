41a5e66e43becdfd28c64ec60a84c7b2d76d896c
Richard Copin <rickcopin@gmail.com>
2017-12-06 10:40:34 -0500

    Delete genome_getter.sh

--- scripts/genome_getter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in `cat S_aureus_gapless_assemblies`; do wget `esearch -db assembly -query "$i" | efetch -format docsum | xtract -pattern DocumentSummary -element FtpPath_GenBank | 
-awk -F"/" '{print $0"/"$NF"_genomic.fna.gz"}'`; done 
-
-gunzip *.gz
-
-
