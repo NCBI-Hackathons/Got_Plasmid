3e9c49129bc9f225c6704cff88bd45c2efd959a7
Richard Copin <rickcopin@gmail.com>
2017-12-06 15:13:37 -0500

    Delete gff_getter.sh

--- scripts/gff_getter.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-for i in cat 'S_aureus_gapless_assemblies'; do wget 'esearch -db assembly -query "$i" | efetch -format docsum | xtract -pattern DocumentSummary -element FtpPath_GenBank | awk -F"/" '{print $0"/"$NF"_genomic.gff.gz"}'; done
-gunzip *.gz
